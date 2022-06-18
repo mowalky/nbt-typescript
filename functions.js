78a4b933394e1c2dbf8d0983d3be5ee486f6a168
Mowalky <mowalky@outlook.com>
2022-06-17 21:15:07 -0500

    updated

---
+++ functions.js
@@ -0,0 +1,36 @@
+"use strict";
+exports.__esModule = true;
+exports.getName = exports.fetchData = exports.printFormat = exports.format = exports.addStrings = void 0;
+function addNumbers(a, b) {
+    return a + b;
+}
+exports["default"] = addNumbers;
+var addStrings = function (str1, str2) {
+    if (str2 === void 0) { str2 = ""; }
+    return str1 + str2;
+};
+exports.addStrings = addStrings;
+// union type
+var format = function (title, params) {
+    return "".concat(title, " ").concat(params);
+};
+exports.format = format;
+var printFormat = function (title, params) {
+    console.log((0, exports.format)(title, params));
+};
+exports.printFormat = printFormat;
+// promise function
+var fetchData = function (url) { return Promise.resolve('data'); };
+exports.fetchData = fetchData;
+function introduce(salutation) {
+    var names = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        names[_i - 1] = arguments[_i];
+    }
+    return "".concat(salutation, " ").concat(names.join(", "));
+}
+function getName(user) {
+    var _a, _b;
+    return "".concat((_a = user === null || user === void 0 ? void 0 : user.first) !== null && _a !== void 0 ? _a : 'first', " ").concat((_b = user === null || user === void 0 ? void 0 : user.last) !== null && _b !== void 0 ? _b : 'last');
+}
+exports.getName = getName;
--- functions.ts
+++ functions.ts
@@ -25,3 +25,7 @@ export const fetchData = (url:string):Promise<string> => Promise.resolve('data')
 function introduce(salutation: string, ...names: string[]): string {
     return `${salutation} ${names.join(", ")}`;
 }
+
+export function getName(user:{first:string,last:string}):string {
+    return `${user?.first ?? 'first'} ${user?.last ?? 'last'}`;
+}
