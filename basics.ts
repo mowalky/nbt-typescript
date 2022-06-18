259af8adeb11d5f2292e8b2ec0e0aea4cfb1a873
Mowalky <mowalky@outlook.com>
2022-06-17 20:47:09 -0500

    first commit

---
+++ basics.ts
@@ -0,0 +1,20 @@
+let userName: string = "Joe Tester";
+let hasLoggedIn:boolean = true;
+
+let myNumber: number = 10;
+
+let myRegex: RegExp = /foo/;
+
+const names: string[] = userName.split(" ")
+const myArray: Array<number> = [1, 2, 3,];
+
+interface Person {
+    first: string;
+    last: string;
+}
+
+const myPerson:Person = {
+    first: "Joe",
+    last: "Tester"
+}
+
