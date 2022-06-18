c920b6c8d8242e6ab5807d4fceb4aa8e6c99fa5d
Mowalky <mowalky@outlook.com>
2022-06-17 21:03:43 -0500

    updated functions

--- functions.ts
+++ functions.ts
@@ -8,3 +8,20 @@ export default addNumbers;
 export const addStrings = (str1:string, str2:string = ""):string => {
     return str1 + str2;
 }
+
+// union type
+export const format = (title:string, params: string|number):string => 
+     `${title} ${params}`;
+
+
+export const printFormat = (title:string, params: string|number):void => {
+    console.log(format(title, params));
+}
+
+// promise function
+export const fetchData = (url:string):Promise<string> => Promise.resolve('data');
+
+
+function introduce(salutation: string, ...names:string[]):string => {
+    return `${salutation} ${names.join(", ")}`;
+}
