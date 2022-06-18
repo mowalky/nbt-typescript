dbe22ab962d7fd44576be7442786b937f931d170
Mowalky <mowalky@outlook.com>
2022-06-17 20:50:39 -0500

    updated

--- basics.ts
+++ basics.ts
@@ -18,3 +18,25 @@ const myPerson:Person = {
     last: "Tester"
 }
 
+// utility type
+const ids: Record<number, string> = {
+    10: "a",
+    20: "b"
+}
+ids[30] = "c"
+
+// conditionals
+
+if(ids[30]==="c") {
+    console.log("d")
+}
+
+// loops (letting typescript infer the type)
+for (let i =0; i < 10; i++) {
+    console.log(i)
+}
+
+[1, 2, 3].forEach(i => console.log(i))
+
+const out = [4, 5, 6].map(v=>v*10)
+
