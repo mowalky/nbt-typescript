ce3051957055ce90902e0c74fa21fb3a67a77c46
Mowalky <mowalky@outlook.com>
2022-06-19 22:37:56 -0500

    optional chaining

--- optionals.ts
+++ optionals.ts
@@ -4,3 +4,21 @@ function printIngredient(quantiy: string, ingredient: string, extra?: string) {
 
 printIngredient("1Cup", "Flour");
 printIngredient("1Cup", "Flour", "Sugar");
+
+interface User {
+  id: string;
+  info?: {
+    email?: string;
+  };
+}
+
+function getEmail(user: User): string {
+  if (user.info) {
+    return user.info.email!;
+  }
+  return "";
+}
+
+function getEmailEasy(user: User): string {
+  return user.info?.email || "";
+}
